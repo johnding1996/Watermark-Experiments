5843d706b631d6339167c37a407201eab930e895
Ubuntu <ubuntu@ip-172-31-21-20.us-east-2.compute.internal>
2023-10-12 22:38:59 +0000

    sync changes on Oct 11

---
+++ scripts/setup.sh
@@ -0,0 +1,19 @@
+# Move to root directory
+cd ..
+# Install and upgrade jupyter
+pip install --upgrade pip ipython jupyter ipywidgets
+# Install dependences (CUDA 12.1)
+pip install torch torchvision torchaudio
+pip install transformers diffusers datasets scikit-learn matplotlib imageio
+# Setup wandb
+pip install wandb
+wandb login e66105607bb979a5e6e49a3d5d4ce02894398354
+# Download datasets
+mkdir datasets && cd datasets
+wget https://s3.amazonaws.com/fast-ai-imageclas/imagenette2-320.tgz && tar -xvzf imagenette2-320. && rm imagenette2-320.tgz
+wget https://image-net.org/data/tiny-imagenet-200.zip && unzip tiny-imagenet-200.zip && rm tiny-imagenet-200.zip
+cd ..
+# Download checkpoints
+mkdir models && cd models
+wget https://openaipublic.blob.core.windows.net/diffusion/jul-2021/64x64_diffusion.pt
+wget https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_diffusion.pt
